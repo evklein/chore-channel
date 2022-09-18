d927b500b023e78950a60fdb0094e0a1bd006e43
hasherr <eklein217@gmail.com>
2022-09-18 00:05:35 -0400

    Dividing into sections

--- src/chores/ChoreCard.tsx
+++ src/chores/ChoreCard.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, CardActions, CardContent, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, Chip, Typography } from "@mui/material";
 import { doc, DocumentData, Firestore, serverTimestamp, updateDoc } from "firebase/firestore";
 
 import "firebase/compat/firestore";
@@ -15,8 +15,53 @@ function ChoreCard(props: ChoreCardProps) {
 
   const updateLastCompleteTime = async() => {
     await updateDoc(doc(firestore, "chores", name), {
-    last_completed: serverTimestamp(),
+      last_completed: serverTimestamp(),
     });
+  }
+
+  const getLastCompletedDate = () => {
+    if (last_completed) {
+        var lastCompletedMs = last_completed.seconds * 1000;
+        return new Date(lastCompletedMs);
+    }
+
+    return new Date();
+  }
+
+  const getPeriodString = () => {
+    switch (period) {
+        case "1":
+            return "Daily";
+        case "7":
+            return "Weekly";
+        case "14":
+            return "Biweekly"
+        case "30":
+            return "Monthly";
+        default:
+            return "Unknown Period";
+    }
+  }
+
+  const getStatusTag = () => {
+    var now = new Date();
+    var lastCompletedDate = getLastCompletedDate();
+    
+    var monthDate = now.getDay();
+
+    if (now.getDay() === lastCompletedDate.getDay() + 1 || (
+        lastCompletedDate.getDay() === 28 || lastCompletedDate.getDay() === 30 || lastCompletedDate.getDay() === 31  &&
+        now.getDay() === 1))
+    {
+        return <Chip label="Do Today" color="warning" sx={{ ml: 1 }} />;
+    }
+    if (now.getDay() >= lastCompletedDate.getDay() + 2 || (
+        lastCompletedDate.getDay() === 28 || lastCompletedDate.getDay() === 30 || lastCompletedDate.getDay() === 31  &&
+        now.getDay() >= 2))
+    {
+        return <Chip label="Overdue" color="error" sx={{ ml: 1 }} />;
+    }
+    return <Chip label="Not Due" color="success" sx={{ ml: 1 }} />;
   }
     
   return (
@@ -26,10 +71,10 @@ function ChoreCard(props: ChoreCardProps) {
           {name}
         </Typography>
         <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          { period }
+          { getPeriodString() }
         </Typography>
         <Typography variant="body2">
-          Last Completed: { last_completed.toLocaleString("en-US", {timeZone: "Etc/GMT+8"}) }
+          Last Completed: { getLastCompletedDate().toLocaleDateString("en-US") }
           <br />
         </Typography>
       </CardContent>
@@ -39,7 +84,7 @@ function ChoreCard(props: ChoreCardProps) {
         }}>
           Completed
         </Button>
-        {/* { getTagForCard() } */}
+        {getStatusTag()}
       </CardActions>
     </Card>
   );
--- src/chores/Chores.tsx
+++ src/chores/Chores.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Divider, Grid, Typography } from "@mui/material";
 import { collection, Firestore, orderBy, query } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import ChoreCard from "./ChoreCard";
@@ -14,13 +14,35 @@ function Chores(props: ChoresProps) {
   ));
 
   return (
-    <Grid container spacing={0}>
-    {chores && chores.map(choreData => (
-      <Grid item xs={12} sm={6} md={3}>
-        <ChoreCard firestore={props.firestore} key={choreData["name"]} choreData={choreData} />
+    <>
+      <Typography variant="h5" align="left" sx={{ marginX: 2, marginY: 1}}>Dailies</Typography>
+      <Grid container spacing={0}>
+      {chores && chores.filter((chore) => { return chore["period"] === 1; }).map(choreData => (
+        <Grid item xs={12} sm={6} md={3}>
+          <ChoreCard firestore={props.firestore} key={choreData["name"]} choreData={choreData} />
+        </Grid>
+      ))}
       </Grid>
-    ))}
-    </Grid>
+      <Divider variant="middle" />
+      <Typography variant="h5" align="left" sx={{ marginX: 2, marginY: 1}}>Weeklies</Typography>
+      <Grid container spacing={0}>
+      {chores && chores.filter((chore) => { return chore["period"] === 7; }).map(choreData => (
+        <Grid item xs={12} sm={6} md={3}>
+          <ChoreCard firestore={props.firestore} key={choreData["name"]} choreData={choreData} />
+        </Grid>
+      ))}
+      </Grid>
+      <Divider variant="middle" />
+      <Typography variant="h5" align="left" sx={{ marginX: 2, marginY: 1}}>Monthlies</Typography>
+      <Grid container spacing={0}>
+      {chores && chores.filter((chore) => { return chore["period"] === 30; }).map(choreData => (
+        <Grid item xs={12} sm={6} md={3}>
+          <ChoreCard firestore={props.firestore} key={choreData["name"]} choreData={choreData} />
+        </Grid>
+      ))}
+      </Grid>
+    </>
+
   );
 }
 
