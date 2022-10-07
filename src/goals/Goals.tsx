98c8b78eee5b4a6062258aed8ea2c173ee48f27d
hasherr <eklein217@gmail.com>
2022-10-07 09:04:21 -0400

    Goals completion

--- src/goals/Goals.tsx
+++ src/goals/Goals.tsx
@@ -1,5 +1,7 @@
 import { Grid, Typography } from "@mui/material";
+import { previousSunday } from "date-fns";
 import { collection, Firestore, orderBy, query } from "firebase/firestore";
+import { useState } from "react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import GoalLogs from "./GoalLogs";
 import GoalSection from "./GoalSection";
@@ -16,10 +18,24 @@ function Goals(props: GoalProps) {
 
   const [events] = useCollectionData(query(
     collection(props.firestore, "events"),
-    orderBy("completed_on")
+    orderBy("completed_on", "desc"),
   ));
 
-  console.log(events);
+  const [currentWeekStartDate] = useState(previousSunday(new Date()));
+
+  const getCompletionRate = () => {
+    var totalGoalsToComplete = 0;
+    var totalGoalsCompleted = 0;
+    goals?.forEach((goal) => {
+      totalGoalsToComplete += goal.required_completions;
+    });
+
+    events?.forEach((event) => {
+      if (event.completed_on.seconds * 1000 > currentWeekStartDate.getTime()) totalGoalsCompleted++;
+    });
+
+    return Math.round(totalGoalsCompleted / totalGoalsToComplete * 100);
+  }
 
   return (
     <>
@@ -28,10 +44,10 @@ function Goals(props: GoalProps) {
                 <Typography variant="h4" align="left" sx={{ marginX: 1 }}>Workouts</Typography>
             </Grid>
             <Grid item md={3}>
-                <Typography variant="h6" align="left">Week of <b>October 2, 2022</b></Typography>
+                <Typography variant="h6" align="left">Week of <b>{currentWeekStartDate.toLocaleDateString()}</b></Typography>
             </Grid>
             <Grid item md={7}>
-                <Typography variant="h5" align="right">Completion: 70%</Typography>
+                <Typography variant="h5" align="right">Completion: {getCompletionRate()}%</Typography>
             </Grid>
         </Grid>
         {goals?.map((goal, index) => {
